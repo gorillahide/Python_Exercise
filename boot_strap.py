16f5c651525f7e8951308da677b5d4ca83166aa3
gorillahide <ajones.sada@gmail.com>
2023-11-15 15:15:36 -0600

    Stole bootstrap

--- boot_strap.py
+++ boot_strap.py
@@ -8,18 +8,18 @@ Created on Wed Oct 25 14:32:28 2023
 
 import pandas as pd
 import matplotlib.pyplot as plt
-
 from plotnine import *
 
-stat = "median"
-dat = pd.DataFrame({"handspan": [20, 20, 19, 24.2, 20, 20.2, 21.5, 17, 19.5,
-                                 21.5, 17, 19.5, 21.5, 18, 18, 20.5,
-                                 20, 20.3, 1.5, 19, 20.4, 22.7, 22.9, 17,
-                                 23, 23.8, 22, 21.5, 21.5]})
-boot_means = []
+dat = pd.read_csv("2017_Fuel_Economy_Data.csv")
+
+dat = dat["Combined Mileage (mpg)"]
+n = len(dat)
+n_boot = 10_000                            
+stat = "mean"
+
 boot_stat = []
-for i in range(10_000):
-    boot_sample = dat.sample(26, replace = True)
+for i in range(n_boot):
+    boot_sample = dat.sample(n, replace = True)
     
     if stat == "median":
         boot_stat.append(float(boot_sample.median()))
@@ -27,13 +27,11 @@ for i in range(10_000):
         boot_stat.append(float(boot_sample.mean()))
     elif stat == "std dev":
         boot_stat.append(float(boot_sample.std()))
-    else: 
-        raise TypeError("Invalid statistic selection")
-
-
+    else:
+        raise TypeError("Wrong statistic name")
 boot_df = pd.DataFrame({'x': boot_stat})
 
 (
- ggplot(boot_df, aes(x = "x")) +
+ ggplot(boot_df, aes(x = "x"))+
  geom_histogram()
  )
