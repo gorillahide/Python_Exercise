129a262e57de807bc663b3fd6a63270b43ab99d1
gorillahide <ajones.sada@gmail.com>
2023-11-15 15:39:32 -0600

    Failed to attempt to implement methods

--- boot_strap.py
+++ boot_strap.py
@@ -16,28 +16,34 @@ the working directory feature on your IDE. Hardcoding isn't going to work.
 """
 
 
-dat = pd.read_csv("2017_Fuel_Economy_Data.csv")
-
-dat = dat["Combined Mileage (mpg)"]
-n = len(dat)
-n_boot = 10_000                            
-stat = "mean"
-
-boot_stat = []
-for i in range(n_boot):
-    boot_sample = dat.sample(n, replace = True)
+class Boot:
+    def __init__(self, stat = "mean", n_boot = 100, dat = None, boot_df = None, ci_level = .95):
+        self.stat = stat
+        self.n_boot = n_boot
+        self.dat = None
+        self.n = len(dat)
+        self.boot_stat = None
+        self.ci_level = ci_level
+        self.boot_df = None
     
-    if stat == "median":
-        boot_stat.append(float(boot_sample.median()))
-    elif stat == "mean":
-        boot_stat.append(float(boot_sample.mean()))
-    elif stat == "std dev":
-        boot_stat.append(float(boot_sample.std()))
-    else:
-        raise TypeError("Wrong statistic name")
-boot_df = pd.DataFrame({'x': boot_stat})
+    def boot_taker(self, n_boot):
+        for i in range(n_boot):
+            boot_sample = dat.sample(n, replace = True)
+            
+            if stat == "median":
+                boot_stat.append(float(boot_sample.median()))
+            elif stat == "mean":
+                boot_stat.append(float(boot_sample.mean()))
+            elif stat == "std dev":
+                boot_stat.append(float(boot_sample.std()))
+            else:
+                raise TypeError("Wrong statistic name")
+    def gen_histogram(self):
+        (
+         ggplot(self.boot_df, aes(x = "x"))+
+         geom_histogram()
+         )
 
-(
- ggplot(boot_df, aes(x = "x"))+
- geom_histogram()
- )
+
+Boot("mean", 100, pd.read_csv("2017_Fuel_Economy_Data.csv"), .95)
+Boot.gen_histogram(Boot)
--- first_prog.py
+++ first_prog.py
@@ -1,4 +1,3 @@
 print("Hello World")
 
 print("You are a brainlet if you cannot use command prompt git")
-saffsasfafssaffsafsafsa
