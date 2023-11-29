af41e3ef106752a8bc7dbf90348ae0ba8b3c4def
gorillahide <ajones.sada@gmail.com>
2023-11-29 15:36:36 -0600

    bootstrap update

--- boot_strap.py
+++ boot_strap.py
@@ -9,6 +9,7 @@ Created on Wed Oct 25 14:32:28 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 from plotnine import *
+import numpy as np
 
 """
 When you inevitably run into errors trying to run this, learn to use
@@ -17,36 +18,45 @@ the working directory feature on your IDE. Hardcoding isn't going to work.
 
 
 class Boot:
-    def __init__(self, stat = "mean", n_boot = 100, dat = None, boot_df = None, ci_level = .95):
+    def __init__(self, stat = "mean", n_boot = 100, dat = None, ci_level = .95):
         self.stat = stat
         self.n_boot = n_boot
         self.dat = dat
         self.n = len(dat)
-        self.boot_stat = None
+        self.boot_stat = stat
         self.ci_level = ci_level
-        self.boot_df = None
         self.sims_list = []
-        
     
-    def boot_taker(self, n_boot):
-        for i in range(n_boot):
-            boot_sample = self.dat.sample(n_boot, replace = True)
-            self.sims_list.append(boot_sample)
-            
+    def boot_taker(self):
+        for i in range(self.n_boot):
+            boot_sample = self.dat.sample(self.n, replace=True)
+    
             if self.stat == "median":
-                self.boot_stat.append(float(boot_sample.median()))
+                self.sims_list.append(float(boot_sample.median()))
             elif self.stat == "mean":
-                self.boot_stat.append(float(boot_sample.mean()))
+                self.sims_list.append(float(boot_sample.mean()))
             elif self.stat == "std dev":
-                self.boot_stat.append(float(boot_sample.std()))
+                self.sims_list.append(float(boot_sample.std()))
             else:
                 raise TypeError("Wrong statistic name")
 
+                    
+    def change_stat(self, stat):
+        if self.stat == "median" or "mean" or "std dev": #check
+            self.boot_stat = stat
+        else:
+            raise TypeError("Wrong statistic name")
+            self.sims_list = []
+
     def gen_histogram(self):
+        boot_df = pd.DataFrame({'x': self.sims_list})
+    
         (
-         ggplot(self.boot_df, aes(x = "x"))+
+         ggplot(boot_df, aes(x = "x"))+
          geom_histogram()
          )
+    
+    
     def clear_sims_list(self):
         self.sims_list = []
 
@@ -57,7 +67,9 @@ class Boot:
         self.dat = dat
         self.n = len(dat)
     
-
-Boot("mean", 100, pd.read_csv("2017_Fuel_Economy_Data.csv"), .95)
-Boot.gen_histogram(Boot)
+dat =  pd.read_csv("2017_Fuel_Economy_Data.csv")
+dat = dat["Combined Mileage (mpg)"]
+strap = Boot("mean", 100, dat, .95)
+strap.boot_taker()
+strap.gen_histogram()
 
